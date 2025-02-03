2387e40fd3e8c27ded2bf57c1ecded2bd4b735df
rgastrock <raphaelgastrock@gmail.com>
2025-02-03 10:19:18 -0500

    download demographics

--- R/data.R
+++ R/data.R
@@ -4,7 +4,7 @@
 getData <- function(){
   
   Reach::downloadOSFdata(repository = 'm5dt4',
-                         filelist = list('data/' =c('control_aligned.zip', 'control_rotated.zip', 'handview_aligned.zip', 'handview_rotated.zip', 'cursorjump_aligned.zip', 'cursorjump_rotated.zip')),
+                         filelist = list('data/' =c('control_aligned.zip', 'control_rotated.zip', 'handview_aligned.zip', 'handview_rotated.zip', 'cursorjump_aligned.zip', 'cursorjump_rotated.zip','demographics.csv')),
                          folder = 'data/',
                          overwrite = TRUE,
                          unzip = TRUE,
